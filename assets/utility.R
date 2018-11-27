66ff314e5d907bb047e4d9580472d2d24704d932
Pri Nonis <priyankara.nonis@berkeley.edu>
2018-11-27 01:04:18 -0800

    32 Pages without Model 3, OVA, and Conclusion

--- assets/utility.R
+++ assets/utility.R
@@ -8,14 +8,14 @@ import <- function(package, devtools = F, version = NA)
         if(!require(package, character.only = T))
         {
             print(paste( 'Installing Package : GHUB :', uri))
-            devtools::install_github(uri)
+            supressWarnings(devtools::install_github(uri))
         }
         else
         {
             if(packageVersion(package) < version)
             {
                 print(paste( ' Upgrading Package : GHUB :', uri))
-                devtools::install_github(uri)
+                supressWarnings(devtools::install_github(uri))
             }
         }
     }
@@ -24,10 +24,20 @@ import <- function(package, devtools = F, version = NA)
         if(!require(package, character.only = T))
         {
             print(paste( 'Installing Package : CRAN :', package))
-
-            install.packages(package, dep = T)
+            supressWarnings(install.packages(package, dep = T))
         }
     }
 
     suppressWarnings(library(package, character.only = T))
+}
+
+pHist <- function(x, breaks, label)
+{
+    ggplot() +
+        geom_histogram(aes(y=..density..,x=x), bins=breaks, fill="yellow", colour="black") +
+        geom_vline(xintercept=mean(x), linetype="dashed", size=1, colour="blue") +
+        stat_function(aes(x=x), fun = dnorm, colour = "red", size = 1,
+                      args=list(mean=mean(x), sd=sd(x))) +
+        xlab(label) +
+        theme_economist()
 }
