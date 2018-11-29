1289f165ee0917e2fb69618c171a989c9c92721b
Pri Nonis <priyankara.s.nonis@intel.com>
2018-11-28 17:24:45 -0800

    Style Updates

--- appendix.R
+++ appendix.R
@@ -1,76 +1,62 @@
-import <- function(package, devtools = F, version = NA)
+# appendix.R
+
+pModel <- function(m, title)
 {
-    if(devtools == T)
-    {
-        uri <- package
-        package <- tail(strsplit(package, '/')[[1]], 1)
+    autoplot(m, smooth.colour = "#dd4814", smooth.size = 1, alpha = 0.8, size = 2) +
+    theme(title = element_text(size = 10, face = 'bold', family = 'sans'),
+          plot.background = element_rect(fill = "#f8f2e4"))
 
-        if(!require(package, character.only = T))
-        {
-            print(paste( 'Installing Package : GHUB :', uri))
-            suppressWarnings(devtools::install_github(uri))
-        }
-        else
-        {
-            if(packageVersion(package) < version)
-            {
-                print(paste( ' Upgrading Package : GHUB :', uri))
-                suppressWarnings(devtools::install_github(uri))
-            }
-        }
-    }
-    else
-    {
-        if(!require(package, character.only = T))
-        {
-            print(paste( 'Installing Package : CRAN :', package))
-            suppressWarnings(install.packages(package, dep = T))
-        }
-    }
-
-    suppressWarnings(library(package, character.only = T))
 }
 
 pHist <- function(x, breaks, label)
 {
     ggplot() +
-        geom_histogram(aes(y=..density..,x=x), bins=breaks, fill="yellow", colour="black") +
-        geom_vline(xintercept=mean(x), linetype="dashed", size=1, colour="blue") +
-        stat_function(aes(x=x), fun = dnorm, colour = "red", size = 1,
-                      args=list(mean=mean(x), sd=sd(x))) +
-        xlab(label) +
-        theme_economist() +
-        scale_colour_economist()
+    geom_histogram(aes(y = ..density.., x = x),
+                   bins = breaks, fill = '#ffbb33', colour = '#000000') +
+    geom_vline(xintercept = mean(x),
+               linetype = 'dashed', size = 1.5, colour = '#0d47a1') +
+    stat_function(aes(x = x), fun = dnorm, colour = '#dd4814',
+                  size = 1.5, args = list(mean = mean(x), sd = sd(x))) +
+    ggtitle(label) + theme_wsj() +
+    theme(title = element_text( size = 10, face = 'bold', family = 'sans'),
+          plot.background = element_rect(fill = "#f8f2e4"))
+}
+
+pScatterMatrix <- function(data, columns)
+{
+    ggscatmat(data, columns = c(26,4,5,6), alpha = 0.5) +
+    geom_smooth(method = 'lm', colour = '#dd4814') + theme_wsj()
 }
 
 mSetup <- function()
 {
-    cfips           <<- county.fips[grepl('north carolina', county.fips$polyname),] # fips for NC counties
-    cfips           <<- rename(cfips, county = fips, subregion = polyname)          # conform column names
-    cfips$county    <<- cfips$county - 37000                                        # conform to crime_v2
-    cfips$subregion <<- gsub('north carolina,(.*?)', '\\1', cfips$subregion)        # remove state name
-    cfips$subregion <<- gsub(':.*','',cfips$subregion)                              # fix currituck county
-    cfips           <<- cfips[!duplicated(cfips$subregion),]                        # del currituck duplicates
-    rownames(cfips) <<- NULL                                                        # reset row names
+    cfips           <<- county.fips[grepl('north carolina', county.fips$polyname),]
+    cfips           <<- rename(cfips, county = fips, subregion = polyname)
+    cfips$county    <<- cfips$county - 37000
+    cfips$subregion <<- gsub('north carolina,(.*?)', '\\1', cfips$subregion)
+    cfips$subregion <<- gsub(':.*','',cfips$subregion)
+    cfips           <<- cfips[!duplicated(cfips$subregion),]
+    rownames(cfips) <<- NULL
 
-    ncmap           <<- subset(map_data('county'), region == 'north carolina') # geo locations for NC counties
-    ncmap           <<- left_join(ncmap, cfips, by = 'subregion')              # append county fips codes
-    rownames(ncmap) <<- NULL                                                   # reset row names
-    ncmap           <<- left_join(ncmap, crime, by = 'county')                 # append crime data
+    ncmap           <<- subset(map_data('county'), region == 'north carolina')
+    ncmap           <<- left_join(ncmap, cfips, by = 'subregion')
+    rownames(ncmap) <<- NULL
+    ncmap           <<- left_join(ncmap, crime, by = 'county')
 }
 
 mMapNC <- function(column, title)
 {
-    ggplot(data = ncmap, mapping = aes_string(x = 'long', y = 'lat', group = 'group', fill = column)) +
-        geom_polygon(color = 'black', size = 0.025) + theme_wsj() +
-        coord_map(projection = "albers", lat0 = 39, lat1 = 45) +
-        theme(legend.position = 'bottom',
-              text = element_text(size = 12, family = 'sans'),
-              title = element_text(size = 16, family = 'sans'),
-              plot.title = element_text(hjust = 0.5),
-              plot.subtitle = element_text(hjust = 0.5)) +
-        ggtitle(title, subtitle = 'North Carolina') +
-        scale_fill_gradientn(colors = rev(brewer.pal(11, 'RdYlGn')))
+    ggplot(data = ncmap,
+           mapping = aes_string(x = 'long', y = 'lat', group = 'group', fill = column)) +
+    geom_polygon(color = 'black', size = 0.025) +
+    coord_map(projection = 'albers', lat0 = 39, lat1 = 45) + theme_wsj() +
+    theme(legend.position = 'bottom',
+          text = element_text(size = 12, family = 'sans'),
+          title = element_text(size = 16, family = 'sans'),
+          plot.title = element_text(hjust = 0.5),
+          plot.subtitle = element_text(hjust = 0.5)) +
+    ggtitle(title, subtitle = 'North Carolina') +
+    scale_fill_gradientn(colors = rev(brewer.pal(11, 'RdYlGn')))
 }
 
 sBuild <- function(dat,out){
@@ -85,14 +71,21 @@ sBuild <- function(dat,out){
 }
 
 sTable <- function(tab){
-    formattable(tab, list(CrimeCOR = color_bar("powderblue"), SD = color_bar("orange"),
+    formattable(tab, list(CrimeCOR = color_bar('#33b5e5'), SD = color_bar('#ff8800'),
                           Spread = function(z){
                               sapply(z, function(zz){
-                                  knit(text = sprintf("`r sparkline(c(%s), type='box')`",
-                                                      paste0(zz, collapse = ",")), quiet = T)})},
+                                  knit(text = sprintf('`r sparkline(c(%s), type="box")`',
+                                                      paste0(zz, collapse = ',')), quiet = T)})},
                           Histogram = function(z){
                               sapply(z, function(zz){
-                                  knit(text = sprintf("`r sparkline(c(%s), type='bar')`",
-                                                      paste0(zz, collapse = ",")), quiet = T)})}
+                                  knit(text = sprintf('`r sparkline(c(%s), type="bar")`',
+                                                      paste0(zz, collapse = ',')), quiet = T)})}
     ))
+}
+
+import <- function(package)
+{
+    if(!require(package, character.only = T))
+        suppressWarnings(install.packages(package, dep = T))
+    suppressWarnings(library(package, character.only = T))
 }
